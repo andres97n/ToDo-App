16e39a5a5cdb1b02b86e051fe12fb692e07e9deb
Andrés Novillo <novilloa21@gmail.com>
2023-09-04 18:03:24 -0500

    Login Page: UI Almost Finished

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+
+  private _fb = inject( FormBuilder );
+
+  public authForm: FormGroup = this._fb.group({ 
+    email: ['', [Validators.required]],
+    password: [
+      '', 
+      [
+        Validators.required, 
+        Validators.minLength(6)
+      ],
+    ]
+  });
+  
+  submit(): void {
+
+    if ( this.authForm.errors ) return;
+
+    const { email, password } = this.authForm.value;
+
+    console.log(email, password);
+       
+  }
 
 }
--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
 
 
 
 @NgModule({
-  exports: [],
+  exports: [
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+  ],
 })
 export class PrimengModule { }
