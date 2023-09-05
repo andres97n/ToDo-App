40b158c5d6a3e28ab28c91e2a88131268756f43c
Andrés Novillo <novilloa21@gmail.com>
2023-09-05 18:40:10 -0500

    ToDo Layout: UI Finished

--- src/app/todo/pages/todo-list-page/todo-list-page.component.ts
+++ src/app/todo/pages/todo-list-page/todo-list-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-list-page',
   templateUrl: './todo-list-page.component.html',
   styleUrls: ['./todo-list-page.component.scss']
 })
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,20 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
+import { PrimengModule } from '../primeng/primeng.module';
+
 import { TodoLayoutComponent } from './layouts/todo-layout/todo-layout.component';
+
 import { TodoPageComponent } from './pages/todo-page/todo-page.component';
 import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.component';
+
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 
 @NgModule({
   declarations: [
     TodoLayoutComponent,
     TodoPageComponent,
-    TodoListPageComponent
+    TodoListPageComponent,
+    FooterComponent,
+    HeaderComponent,
+    TodoCardComponent
   ],
   imports: [
     CommonModule,
-    TodoRoutingModule
+
+    TodoRoutingModule,
+    PrimengModule,
   ]
 })
 export class TodoModule { }
